d309d46d2c61451b62c1922928d90d94cd598a91
tonyZio <95496314+tonyZio@users.noreply.github.com>
2023-10-02 21:15:56 -0700

    Se agrego creacion de bd al script del .sql

--- Bases Datos/Consejeria_Demandas.sql
+++ Bases Datos/Consejeria_Demandas.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `consejeria_demandas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `consejeria_demandas`;
 -- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
 --
 -- Host: 127.0.0.1    Database: consejeria_demandas
