1e4d37c725a5900d40ef534166a82a54e7215f2e
MarcoIrineo <marco.irineo228880@potros.itson.edu.mx>
2023-10-03 16:08:51 -0700

    2 daos
    se realizaron la dao de denuncia y participante

---
+++ Consejeria Demandas/data-access/denunciaDAO.js
@@ -0,0 +1,53 @@
+const {Denuncia} = require('../models');
+class DenunciaDAO{
+    constructor(){}
+
+    async crearDenuncia({id_proceso_judicial,causa_penal, delito, modalidad,hechos,plazo_cierre, unidad_mpi, estrategia,id_juez}){
+        try{
+            const denuncia = await Denuncia.create({id_proceso_judicial,causa_penal, delito, modalidad, hechos, plazo_cierre, unidad_mpi,estrategia, id_juez});
+            return denuncia;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async obtenerDenuncias(){
+        try{
+            const denuncias = await Denuncia.findAll();
+            return denuncias;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async obtenerDenunciaPorId(id){
+        try{
+            const denuncia = await Denuncia.findByPk(id);
+            return denuncia;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async actualizarDenuncia(id, {id_proceso_judicial,causa_penal, delito, modalidad, hechos, plazo_cierre, unidad_mpi, estrategia, id_juez}){
+        try{
+            const denuncia = await Denuncia.update({id_proceso_judicial,causa_penal, delito, modalidad, hechos, plazo_cierre, unidad_mpi,estrategia, id_juez}, {where: {id}});
+            return denuncia;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async eliminarDenuncia(id){
+        try{    
+            const denuncia = await Denuncia.findByPk(id);
+            if(!denuncia){
+                throw new Error('Denuncia no encontrada');    
+            }
+            await denuncia.destroy();
+            return true;
+        }catch(e){
+            throw e;
+        }
+    }
+}
---
+++ Consejeria Demandas/data-access/participanteDAO.js
@@ -0,0 +1,54 @@
+const {Participante} = require('../models');
+
+class ParticipanteDAO{
+    constructor(){}
+
+    async crearParticipante({nombre, edad, DTYPE, id_escolaridad, id_etnia, id_ocupacion, id_persona}){
+        try{
+            const participante = await Participante.create({nombre, edad, DTYPE, id_escolaridad, id_etnia, id_ocupacion, id_persona});
+            return participante;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async obtenerParticipantes(){
+        try{
+            const participantes = await Participante.findAll();
+            return participantes;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async obtenerParticipantePorId(id){
+        try{
+            const participante = await Participante.findByPk(id);
+            return participante;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async actualizarParticipante(id, {nombre, edad, DTYPE, id_escolaridad, id_etnia, id_ocupacion, id_persona}){
+        try{
+            const participante = await Participante.update({nombre, edad, DTYPE, id_escolaridad, id_etnia, id_ocupacion, id_persona}, {where: {id}});
+            return participante;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    async eliminarParticipante(id){
+        try{
+            const participante = await Participante.findByPk(id)
+            if(!participante){
+                throw new Error('No existe el participante');
+            }
+            await participante.destroy();
+            return 'participante eliminado con exito';
+        }catch(err){
+            throw err;
+        }
+    }
+}
