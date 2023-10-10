c26a62b27e3412ccba7f13c2a4f6ae1afb7b7261
WhitheSpaceJJ <101837332+WhitheSpaceJJ@users.noreply.github.com>
2023-10-10 13:56:00 -0700

    Delete Consejeria Asesorias directory

--- Consejeria Asesorias/modelos/modeloPersona.js
+++
@@ -1,4 +0,0 @@
-const { Persona, Domicilio, Genero } = require("../utilidades/modelosBase");
-Persona.hasOne(Domicilio, { foreignKey: "id_domicilio" })
-Persona.hasOne(Genero, { foreignKey: "id_genero" })
-module.exports = { Persona ,Domicilio,Genero};
