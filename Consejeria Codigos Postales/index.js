25caf6aa96d69a1456ed04b9da30acacb82aa1df
WhitheSpaceJJ <101837332+WhitheSpaceJJ@users.noreply.github.com>
2023-10-10 13:30:18 -0700

    Delete Consejeria Codigos Postales directory

--- Consejeria Codigos Postales/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./src/app'); // Importamos el modulo app
-const { PORT } = require('./src/config/default'); // Importamos el puerto desde config/default.js
-
-app.listen(PORT, () => { // Iniciamos el servidor
-    console.log(`Server running on port ${PORT}`); // Mostramos un mensaje en la consola
-});
