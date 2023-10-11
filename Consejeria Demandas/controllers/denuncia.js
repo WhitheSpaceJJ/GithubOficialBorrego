8fcbff60426466681c5cdbae488f7b31cfc31df0
MarcoIrineo <marco.irineo228880@potros.itson.edu.mx>
2023-10-10 20:32:11 -0700

    3 controladores
    se crearon los controladores de demanda, denuncia y escolaridad

---
+++ Consejeria Demandas/controllers/denuncia.js
@@ -0,0 +1,72 @@
+const denunciaDAO = require('../data-access/denunciaDAO')
+
+const obtenerDenuncias = async (_, res) => {
+  try {
+    const denuncias = await denunciaDAO.obtenerDenuncias()
+    res.json(denuncias)
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al realizar la consulta con bd'
+    })
+  }
+}
+
+const obtenerDenuncia = async (req, res) => {
+  try {
+    const { id } = req.params
+    const denuncia = await denunciaDAO.obtenerDenunciaPorId(Number(id))
+    res.json(denuncia)
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al realizar la consulta con bd'
+    })
+  }
+}
+
+const crearDenuncia = async (req, res) => {
+  try {
+    const { id_proceso_judicial, causa_penal, delito, modalidad, hechos, plazo_cierre, unidad_mpi, estrategia, id_juez } = req.body
+
+    const denuncia = await denunciaDAO.crearDenuncia({ id_proceso_judicial, causa_penal, delito, modalidad, hechos, plazo_cierre, unidad_mpi, estrategia, id_juez })
+
+    res.json(denuncia)
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al realizar la consulta con bd'
+    })
+  }
+}
+
+const actualizarDenuncia = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { id_denuncia, ...data} = req.body
+    await denunciaDAO.actualizarDenuncia(Number(id), data)
+    const denuncia = await denunciaDAO.obtenerDenunciaPorId(Number(id))
+    res.json(denuncia)
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al realizar la consulta con bd'
+    })
+  }
+}
+
+const eliminarDenuncia = async (req, res) => {
+  try {
+    const { id } = req.params
+    const denuncia = denunciaDAO.eliminarDenuncia(Number(id))
+    res.json(denuncia)
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al realizar la consulta con bd'
+    })
+  }
+}
+
+module.exports = {
+  obtenerDenuncias,
+  crearDenuncia,
+  obtenerDenuncia,
+  actualizarDenuncia,
+  eliminarDenuncia
+}
