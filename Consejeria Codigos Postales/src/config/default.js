897f54a2948ee0d8fe071cfbf363ff986208f02f
WhiteSpaceJJ <jose.orozco229141@potros.itson.edu.mx>
2023-10-13 00:46:27 -0700

    igual que en el servicio montado

--- Consejeria Codigos Postales/src/config/default.js
+++ Consejeria Codigos Postales/src/config/default.js
@@ -9,7 +9,7 @@ const DBHOST = process.env.DBHOST || 'localhost'; // Obtenemos el valor de la va
 const DBUSER = process.env.DBUSER || 'root'; // Obtenemos el valor de la variable de entorno DBUSER o usamos root por defecto
 const DBPASSWORD = process.env.DBPASSWORD || '1234'; // Obtenemos el valor de la variable de entorno DBPASSWORD o usamos 1234 por defecto
 const DATABASE = process.env.DATABASE || 'consejeria_codigos_postales'; // Obtenemos el valor de la variable de entorno DATABASE o usamos consejeria por defecto
-const PORT = process.env.PORT || 3000; // Obtenemos el valor de la variable de entorno PORT o usamos el puerto 3000 por defecto
+const PORT = process.env.PORT || 3012; // Obtenemos el valor de la variable de entorno PORT o usamos el puerto 3000 por defecto
 const DBPORT = process.env.DBPORT || 3306; // Obtenemos el valor de la variable de entorno DBPORT o usamos el puerto 3306 por defecto
 
 module.exports = {
