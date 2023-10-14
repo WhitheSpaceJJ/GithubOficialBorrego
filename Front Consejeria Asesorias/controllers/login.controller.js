7e234e04acd9321a5bad7297dcc119047c4a9bef
Erick Bernal Amparano <erick082016@gmail.com>
2023-10-14 14:36:09 -0700

    feat: add fields validations

--- Front Consejeria Asesorias/controllers/login.controller.js
+++ Front Consejeria Asesorias/controllers/login.controller.js
@@ -1,3 +1,5 @@
+import { validateNonEmptyFields } from "../lib/utils"
+
 class LoginController {
   constructor(model) {
     this.model = model
@@ -8,11 +10,16 @@ class LoginController {
     const correo = document.getElementById("correo").value
     const password = document.getElementById("password").value
     try {
+      if (!validateNonEmptyFields([correo, password])) {
+        throw new Error("Campos obligatorios en blanco, por favor revise.")
+      }
+
       const user = await this.model.login({ correo, password })
       sessionStorage.setItem("user", JSON.stringify(user))
       window.location.replace("index.html")
     } catch (error) {
       const miAlerta = document.getElementById("alerta")
+      document.getElementById("mensaje-alerta").innerHTML = error.message
       miAlerta.style.display = "flex"
       //console.error("Error:", error.message)
     }
