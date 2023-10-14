a4ee4acbf50948f3622ce807258089d0df10bd53
Erick Bernal Amparano <erick082016@gmail.com>
2023-10-14 14:14:07 -0700

    feat: add api request to model

--- Front Consejeria Asesorias/models/api.model.js
+++ Front Consejeria Asesorias/models/api.model.js
@@ -1,17 +1,58 @@
 class APIModel {
-  apiUrl = "http://localhost:3002"
+  USERS_API_URL = "http://localhost:3002"
+  ASESORIAS_API_URL = "http://localhost:3000"
+  CP_API_URL = "http://localhost:3001"
   user = JSON.parse(sessionStorage.getItem("user"))
 
   constructor() {}
 
   //GET methods
 
-  async login(correo, password) {
-    const url = `${this.apiUrl}/usuarios/usuario?correo=${correo}&password=${password}`
+  async login({ correo, password }) {
+    const url = `${this.USERS_API_URL}/usuarios/usuario?correo=${correo}&password=${password}`
     const response = await fetch(url, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
+      },
+    })
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    } else {
+      throw new Error("Error en la petición")
+    }
+  }
+
+  async getAsesoriaByFullName({ nombre, apellidoPaterno, apellidoMaterno }) {
+    const url = new URL(`${this.ASESORIAS_API_URL}/asesorias/buscar`)
+    const params = {
+      nombre: nombre,
+      apellido_paterno: apellidoPaterno,
+      apellido_materno: apellidoMaterno,
+    }
+    url.search = new URLSearchParams(params).toString()
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${this.user.token}`,
+      },
+    })
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    } else {
+      throw new Error("Error en la petición")
+    }
+  }
+
+  async getColoniaById(idColonia) {
+    const url = `${this.CP_API_URL}/colonias/${idColonia}`
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${this.user.token}`,
       },
     })
     if (response.ok) {
