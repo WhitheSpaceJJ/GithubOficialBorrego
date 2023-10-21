743488c832e1aceb25b5acdd61474db9e709f1c9
WhiteSpaceJJ <jose.orozco229141@potros.itson.edu.mx>
2023-10-21 00:48:05 -0700

    de has one to belongs to

--- Consejeria Usuarios/modelos/modeloUsuario.js
+++ Consejeria Usuarios/modelos/modeloUsuario.js
@@ -1,4 +1,4 @@
 const {TipoUser,Zona,Usuario}=require("../utilidades/modelosBase");
 Usuario.belongsTo(TipoUser,{foreignKey:"id_tipouser"});
-Usuario.hasOne(Zona,{foreignKey:"id_zona"})
+Usuario.belongsTo(Zona,{foreignKey:"id_zona"})
 module.exports = {TipoUser,Zona,Usuario};
