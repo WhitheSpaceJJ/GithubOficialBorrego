d60b14a202a2e1a4a73ed2edad4b039012c9d3b7
WhiteSpaceJJ <jose.orozco229141@potros.itson.edu.mx>
2023-10-21 00:54:00 -0700

    modelo

--- Consejeria Asesorias/modelos/modeloAsesoria.js
+++ Consejeria Asesorias/modelos/modeloAsesoria.js
@@ -5,7 +5,7 @@ const {Asesoria,Asesor,Turno,Asesorado,
 Asesoria.belongsTo(Asesor, { foreignKey: "id_asesor" })
 Asesoria.belongsTo(Turno, { foreignKey: "id_turno" })
 Asesoria.belongsTo(Asesorado, { foreignKey: "id_asesorado"})
-Asesoria.belongsToMany(DetalleAsesoriaCatalogo,{foreignKey:"id_asesoria"});
+Asesoria.hasMany(DetalleAsesoriaCatalogo,{foreignKey:"id_asesoria"});
 Asesoria.belongsTo(TipoJuicio, { foreignKey: "id_tipo_juicio" })
 
 module.exports = {Asesoria
