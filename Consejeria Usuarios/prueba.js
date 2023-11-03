f07e040961a6e3aa0196085e8673acfd90740e38
WhiteSpaceJJ <jose.orozco229141@potros.itson.edu.mx>
2023-11-03 00:10:08 -0700

    lineas de codigo inutiles que estaban comentadas

--- Consejeria Usuarios/prueba.js
+++
@@ -1,9 +0,0 @@
-const controlPersonas = require('./controles/controlPersonas');
-
-controlPersonas.obtenerPersonaNombre("JOSE JESUS", "OROZCO","HERNANDEZ")
-.then((result) => {
-    console.log(
-        result);
-}).catch((error) => {
-    console.log(error);
-})
