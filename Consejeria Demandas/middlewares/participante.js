a96e41be0d5e7c49f758a600691460e8bb79466a
Vastem <vastemxt@gmail.com>
2023-11-13 16:31:16 -0700

    fix: agregar participante
    se corrigio para agregar participantes.

--- Consejeria Demandas/middlewares/participante.js
+++ Consejeria Demandas/middlewares/participante.js
@@ -4,7 +4,7 @@ const etniaDAO = require('../data-access/etniaDAO')
 const ocupacionDAO = require('../data-access/ocupacionDAO')
 
 
-async function existeParticipante (req, res, next) {
+async function existeParticipante(req, res, next) {
   const { id } = req.params
   const participante = await participanteDAO.obtenerParticipante(id)
   if (!participante) {
@@ -15,9 +15,9 @@ async function existeParticipante (req, res, next) {
   next()
 }
 
-async function existeEscolaridad (req, res, next) {
+async function existeEscolaridad(req, res, next) {
   const { id_escolaridad } = req.body
-  const escolaridad = await escolaridadDAO.obtenerescolaridad(id_escolaridad)
+  const escolaridad = await escolaridadDAO.obtenerEscolaridadPorId(id_escolaridad)
   if (!escolaridad) {
     return res.status(404).json({
       message: 'No existe un proceso judicial con ese id'
@@ -26,26 +26,26 @@ async function existeEscolaridad (req, res, next) {
   next()
 }
 
-async function existeEtnia (req, res, next) {
-    const { id_etnia } = req.body
-    const etnia = await etniaDAO.obtenerEtnia(id_etnia)
-    if (!etnia) {
-      return res.status(404).json({
-        message: 'No existe una etnia con ese id'
-      })
-    }
-    next()
+async function existeEtnia(req, res, next) {
+  const { id_etnia } = req.body
+  const etnia = await etniaDAO.obtenerEtnia(id_etnia)
+  if (!etnia) {
+    return res.status(404).json({
+      message: 'No existe una etnia con ese id'
+    })
   }
+  next()
+}
 
-async function existeOcupacion (req, res, next) {
-    const { id_ocupacion } = req.body
-    const ocupacion = await ocupacionDAO.obtenerOcupacion(id_ocupacion)
-    if (!ocupacion) {
-      return res.status(404).json({
-        message: 'No existe una ocupacion con ese id'
-      })
-    }
-    next()
+async function existeOcupacion(req, res, next) {
+  const { id_ocupacion } = req.body
+  const ocupacion = await ocupacionDAO.obtenerOcupacion(id_ocupacion)
+  if (!ocupacion) {
+    return res.status(404).json({
+      message: 'No existe una ocupacion con ese id'
+    })
+  }
+  next()
 }
 
 module.exports = {
