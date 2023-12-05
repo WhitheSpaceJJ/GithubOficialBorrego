9c3382224157afb7b436c6b97474f32c498b952a
German <german_gomez1205@hotmail.com>
2023-12-04 20:54:11 -0700

    feat. filtros asesorias

--- Front Consejeria Asesorias/views/consulta.view.js
+++ Front Consejeria Asesorias/views/consulta.view.js
@@ -5,9 +5,18 @@ import '../components/navbar/navbar.js'
 class ConsultaView {
   constructor(controller) {
     this.controller = controller
+    this.filtrosForm = document.getElementById('filtros-form')
+
+    this.filtrosForm.addEventListener('submit', e => {
+      e.preventDefault()
+      this.controller.handleFiltros()
+    })
+
     document.addEventListener(
       'DOMContentLoaded',
-      this.controller.handleDOMContentLoaded()
+      this.controller.handleDOMContentLoaded(),
+      this.controller.handleCheckboxChange(),
+      this.controller.handleSelectChange()
     )
   }
 }
